fa646caabd017cd46635e06bde5fa2c82a76e1a2
markxh <markharper.sa@gmail.com>
2025-04-14 08:26:46 +0200

    [feature] Added app icon

--- composeApp/src/androidMain/kotlin/za/co/markxh/backpacklifesim/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/za/co/markxh/backpacklifesim/MainActivity.kt
@@ -3,15 +3,23 @@ package za.co.markxh.backpacklifesim
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import za.co.markxh.backpacklifesim.ui.AndroidBackpackScreen
+import za.co.markxh.backpacklifesim.ui.BackpackViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: BackpackViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            App()
+            MaterialTheme {
+                AndroidBackpackScreen()
+            }
         }
     }
 }
